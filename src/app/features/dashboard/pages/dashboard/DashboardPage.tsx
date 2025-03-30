4c420fca67f52c8089e16b7e5da0a57483b62786
Jose Perez <joseperu2503@gmail.com>
2025-03-30 12:41:36 -0500

    sidebar

--- src/app/features/dashboard/pages/dashboard/DashboardPage.tsx
+++ src/app/features/dashboard/pages/dashboard/DashboardPage.tsx
@@ -1,11 +1,72 @@
+import account from "../../../../../assets/icons/account.svg";
+import calendar from "../../../../../assets/icons/calendar.svg";
+import contacts from "../../../../../assets/icons/contacts.svg";
+import dashboard from "../../../../../assets/icons/dashboard.svg";
+import logo2 from "../../../../../assets/icons/logo-2.svg";
+import logout from "../../../../../assets/icons/logout.svg";
 import noPets from "../../../../../assets/icons/no-pets.svg";
+import plus from "../../../../../assets/icons/plus.svg";
+import settings from "../../../../../assets/icons/settings.svg";
+import profile from "../../../../../assets/images/profile.png";
 import { Button } from "../../../../shared/components/button/Button";
 import styles from "./DashboardPage.module.scss";
 
 export function DashboardPage() {
   return (
     <div className={styles.background}>
-      <div className={styles.sidebar}></div>
+      <div className={styles.sidebar}>
+        <section className={styles.logoContainer}>
+          <img src={logo2} alt="logo" />
+        </section>
+        <hr />
+        <section className={styles.pets}>
+          <span className={styles.title}>Your Pets</span>
+          <div className={styles.petsList}>
+            <div className={styles.pet}>
+              <button>
+                <img src={plus} alt="plus" className={styles.plus} />
+              </button>
+              <span>add new</span>
+            </div>
+          </div>
+        </section>
+        <hr />
+
+        <div className={styles.links}>
+          <a className={styles.active}>
+            <img src={dashboard} alt="dashboard" />
+            <span>Dashboard</span>
+          </a>
+          <a href="#">
+            <img src={contacts} alt="contacts" />
+            <span>Contacts</span>
+          </a>
+          <a href="#">
+            <img src={calendar} alt="calendar" />
+            <span>Calendar</span>
+          </a>
+        </div>
+        <hr />
+        <ul className={styles.links}>
+          <a href="#">
+            <img src={account} alt="account" />
+            <span>Account</span>
+          </a>
+          <a href="#">
+            <img src={settings} alt="settings" />
+            <span>Settings</span>
+          </a>
+        </ul>
+        <div className={styles.profile}>
+          <img src={profile} alt="" className={styles.profileImage} />
+          <div className={styles.profileName}>
+            <span className={styles.greeting}>Hello</span>
+            <span className={styles.profileNameText}>Esther</span>
+          </div>
+
+          <img src={logout} alt="" className={styles.logout} />
+        </div>
+      </div>
       <div className={styles.content}>
         <div className={styles.card}>
           <div className={styles.noPets}>
