db6832b5688a6cdf9bc825a3df7716074e3bc3ac
Jose Perez <joseperu2503@gmail.com>
2025-11-02 17:09:31 -0500

    Update DashboardPage.tsx

--- src/app/features/dashboard/pages/dashboard/DashboardPage.tsx
+++ src/app/features/dashboard/pages/dashboard/DashboardPage.tsx
@@ -1,11 +1,11 @@
-import { PetProfile } from "../../components/pet-profile/PetProfile";
+import { NoPets } from "../../components/no-pets/NoPets";
 import styles from "./DashboardPage.module.scss";
 
 export function DashboardPage() {
   return (
     <div className={styles.dashboard}>
-      {/* <NoPets></NoPets> */}
-      <PetProfile></PetProfile>
+      <NoPets></NoPets>
+      {/* <PetProfile></PetProfile> */}
     </div>
   );
 }
